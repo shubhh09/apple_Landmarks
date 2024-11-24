4bb90280d52eea2704ffd1b113cb65a970d816b3
SHUBHAM <apple@APPLEs-MacBook-Pro.local>
2024-11-24 20:56:13 +0530

    drawing paths and shapes & animating views and transitions

---
+++ Lanndmarks_apple/View/Badge/RotatedBadgeSymbol.swift
@@ -0,0 +1,21 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Lanndmarks_apple
+//
+//  Created by SHUBHAM on 21/11/24.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle:Angle
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
